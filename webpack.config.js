608da5eb7ad00903a3bcb1793c5a44e4d65b1fb4
daitendev <exiad.iagan@gmail.com>
2021-06-08 23:22:29 +0900

    apply optimize-css-assetswebpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const UglifyJsWebPackPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCssAssetsWebPackPlugin = require('optimize-css-assets-webpack-plugin')
 
 const outputPath = path.resolve(__dirname, 'dist')
 
@@ -66,7 +67,8 @@ module.exports = {
             }
           }
         }
-      )
+      ),
+      new OptimizeCssAssetsWebPackPlugin()
     ]
   }
 }
